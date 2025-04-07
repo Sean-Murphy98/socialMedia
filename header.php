19e2e2e06ee8196c8c7cde236220130917cc44bc
Sean-Murphy98 <seangm4.murphy@gmail.com>
2025-04-06 18:12:11 -0700

    Finished introductory work laid out in "Learning PHP, MySQL, JavaScript, CSS & HTML" by Robin Nixon

--- header.php
+++ header.php
@@ -15,10 +15,10 @@
     }
     else $loggedin = FALSE;
 
-    echo "<title>$appname$userstr</title> rel='stylesheet'" . 
+    echo "<title>$appname$userstr</title><link rel='stylesheet'" . 
          "href='styles.css' type='text/css'>" . 
          "</head><body><center><canvas id='logo' width='624' " .
-         "height='96'>$appname</canvas></center>" .
+         "height='120'>$appname</canvas></center>" .
          "<div class='appname'>$appname$userstr</div>" . 
          "<script src='javascript.js'></script>";
     
@@ -35,6 +35,6 @@
     echo "<br ><ul class='menu'>" .
         "<li><a href='index.php'>Home</a></li>" .
         "<li><a href='signup.php'>Sign up</a></li>" .
-        "<li><a href='login.php'>Log in</a></li>" .
+        "<li><a href='login.php'>Log in</a></li></ul><br>" .
         "<span class='info'>&#8658; You must be logged in to view this page. </span><br><br>";
 ?>
